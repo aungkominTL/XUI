dd171a4abf909c8ec594aee26caafe0e80ec107f
Aung Ko Min <aung_ko_min@transitlink.com.sg>
2023-02-18 23:07:38 +0800

    1.0.4

--- Sources/XUI/SwiftUI/FormCell.swift
+++ Sources/XUI/SwiftUI/FormCell.swift
@@ -7,11 +7,16 @@
 
 import SwiftUI
 @available(iOS 16.0, *)
+
 public struct FormCell<Left: View, Right: View>: View {
     
-    @ViewBuilder public var left: () -> Left
-    @ViewBuilder public var right: () -> Right
+    @ViewBuilder var left: () -> Left
+    @ViewBuilder var right: () -> Right
 
+    init(left: @escaping () -> View, right: @escaping () -> View) {
+        self.left = left
+        self.right = right
+    }
     public var body: some View {
         HStack {
             left()
