40b4259c2a1a9709b068931a75d332c40a4cd880
Aung Ko Min <aung_ko_min@transitlink.com.sg>
2023-02-19 11:43:59 +0800

    1.1.0

---
+++ Sources/XUI/Utils/DateFormatters.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aung Ko Min on 19/2/23.
+//
+
+import Foundation
+
+public extension Date {
+
+    func timeAgo() -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .full
+        formatter.allowedUnits = [.year, .month, .day, .hour, .minute, .second]
+        formatter.zeroFormattingBehavior = .dropAll
+        formatter.maximumUnitCount = 1
+        return String(format: formatter.string(from: self, to: Date()) ?? "", locale: .current)
+    }
+}
