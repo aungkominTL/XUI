d19a68beda555ed51aeaf44ce2a4031fba148cf7
Aung Ko Min <aung_ko_min@transitlink.com.sg>
2023-02-21 15:43:28 +0800

    1.1.4

---
+++ Sources/XUI/SwiftUI/Views/NumberTextField.swift
@@ -0,0 +1,42 @@
+//
+//  IntTextField.swift
+//  HomeForYou
+//
+//  Created by Aung Ko Min on 30/1/23.
+//
+
+import SwiftUI
+@available(iOS 16.0, *)
+public struct _NumberTextField: View {
+
+    private var value: Binding<Int>
+    private let title: String
+    private let delima: String?
+
+    public init(value: Binding<Int>, title: String, delima: String? = nil) {
+        self.value = value
+        self.title = title
+        self.delima = delima
+    }
+
+    private func get() -> String {
+        if value.wrappedValue == 0 { return String() }
+        if let delima {
+            return "\(delima)\(value)"
+        }
+        return "\(value)"
+    }
+
+    private func set(_ newValue: String) {
+        if let delima {
+            value.wrappedValue = Int(newValue.replace(delima, with: String())) ?? 0
+        } else {
+            value.wrappedValue = Int(newValue) ?? 0
+        }
+    }
+
+    public var body: some View {
+        TextField("Please input the \(title)", text: .init(get: get, set: set(_:)))
+            .keyboardType(.numberPad)
+    }
+}
