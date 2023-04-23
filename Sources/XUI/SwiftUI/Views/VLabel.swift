70c339b4ac4117d814323498088cba825ceebff5
Aung Ko Min <aung_ko_min@transitlink.com.sg>
2023-04-23 10:33:58 +0800

    23/4

---
+++ Sources/XUI/SwiftUI/Views/VLabel.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aung Ko Min on 23/4/23.
+//
+
+import SwiftUI
+
+@available(iOS 16.0, *)
+
+public struct _VLabel<Top, Bottom>: View where Top: View, Bottom: View {
+
+    private let spacing: CGFloat
+    private var top: () -> Top
+    private var bottom: () -> Bottom
+
+    public init(spacing: CGFloat = 5, @ViewBuilder top: @escaping () -> Top, @ViewBuilder bottom: @escaping () -> Bottom) {
+        self.spacing = spacing
+        self.top = top
+        self.bottom = bottom
+    }
+    public init(spacing: CGFloat = 5, iconName: String, text: String) {
+        self.spacing = spacing
+        self.top = {
+            Image(systemName: iconName) as! Top
+        }
+        self.bottom = {
+            Text(text) as! Bottom
+        }
+    }
+    public var body: some View {
+        VStack(alignment: .leading, spacing: spacing) {
+            top()
+                .imageScale(.small)
+            bottom()
+        }
+    }
+}
